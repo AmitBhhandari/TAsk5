1fb4fec8590e5470b9184edafb11cdaaeb9cb07e
AmitBhandari <amitbhandari68@gmail.com>
2022-10-18 14:26:25 +0530

    TAsk05 added

---
+++ file2.js
@@ -0,0 +1,12 @@
+var li=document.getElementsByTagName('li');
+//for(var i=0;i<li.length;i++)
+//{
+   // li[i].style.color='red';
+//}
+
+console.log(li)
+li[1].textContent='HELLO'
+li[2].style.color='red';
+
+var NewLi=Document.getElementsByClassName('new-li-element')
+NewLi[0].style.color='red'
